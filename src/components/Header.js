36933c61ce4b9157f8f25124cd8282ed4ee6cdf0
unknown <sahilsiddiqui.dev@gmail.com>
2020-09-26 14:18:50 +0530

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+//import { Link } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import SearchIcon from "@material-ui/icons/Search";
+import Avatar from "@material-ui/core/Avatar";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1280px-Logo_of_YouTube_%282015-2017%29.svg.png"
+          atl="logo"
+        />
+      </div>
+
+      <div className="header__input">
+        <input type="text" />
+        <SearchIcon className="header__inputButton" />
+      </div>
+
+      <div className="header__icons">
+        <VideoCallIcon />
+        <AppsIcon />
+        <NotificationsIcon />
+        <Avatar alt="user" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
